dc5f96d89da1055a783e5b143d0f16b5f0655b7b
Kenny <lamkiyinkenny2001@gmail.com>
2017-10-21 17:13:31 -0700

    Recognition success

--- js/script.js
+++ js/script.js
@@ -28,106 +28,135 @@ function permission(){
      }
   );
 }
-if (!('webkitSpeechRecognition' in window)) {
-  console.log("need webkitSpeechRecognition");
-  //upgrade();
-}
-else {
-  console.log("has webkitSpeechRecognition");
-  permission();
-  var recognition = new webkitSpeechRecognition();
-  recognition.lang = "en-US";
-  //When user stop talking, recognition will end
-  recognition.continuous = false;
-  recognition.interimResults = true;
-  // final_span.innerHTML = '';
-  // interim_span.innerHTML = '';
-  // start_img.src = 'mic-slash.gif';
-  // showInfo('info_allow');
-  // showButtons('none');
-  // start_timestamp = event.timeStamp;
-  recognition.onstart = function() {
-    console.log("in onstart");
-    start_time = (new Date()).getTime();
-    recognizing = true;
-    var x = setInterval(function(){
-      // recognition.stop();
-      recognition.stop();
-      console.log("end onstart");
-      clearInterval(x);
-    },5000);
 
+ask_question();
+// speech_recognition(temp);
+
+function speech_recognition(answer){
+  if (!('webkitSpeechRecognition' in window)) {
+    console.log("need webkitSpeechRecognition");
+    //upgrade();
   }
-  recognition.onresult = function(event) {
-      console.log("in onresult");
-      msg = ''
-      if((new Date).getTime() - start_time > 5000){
-        console.log("expire");
-        return;
-      }
-      for (var i = event.resultIndex; i < event.results.length; i++){
-          if(event.results[i].isFinal){
-              msg += event.results[i][0].transcript;
-          }
-      }
-      console.log("msg = " + msg);
-      if(msg != ''){
-        document.getElementById('msg').innerHTML = msg;
-        if(msg.toUpperCase() == "PLUS"){
-          console.log("+");
+  else {
+    console.log("has webkitSpeechRecognition");
+    permission();
+    var recognition = new webkitSpeechRecognition();
+    recognition.lang = "en-US";
+    //When user stop talking, recognition will end
+    recognition.continuous = false;
+    recognition.interimResults = true;
+    // final_span.innerHTML = '';
+    // interim_span.innerHTML = '';
+    // start_img.src = 'mic-slash.gif';
+    // showInfo('info_allow');
+    // showButtons('none');
+    // start_timestamp = event.timeStamp;
+    recognition.onstart = function() {
+      console.log("in onstart");
+      start_time = (new Date()).getTime();
+      recognizing = true;
+      var x = setInterval(function(){
+        // recognition.stop();
+        recognition.stop();
+        console.log("end onstart");
+        clearInterval(x);
+      },6000);
+    }
+    recognition.onresult = function(event) {
+        console.log("in onresult");
+        msg = ''
+        if((new Date).getTime() - start_time > 5000){
+          console.log("expire");
+          return;
         }
-        else if(msg.toUpperCase() == "MINUS"){
-          console.log("-");
+        for (var i = event.resultIndex; i < event.results.length; i++){
+            if(event.results[i].isFinal){
+                msg += event.results[i][0].transcript;
+            }
         }
+        console.log("msg = " + msg);
+         if(msg != ''){
+            document.getElementById('msg').innerHTML = msg;
+        //   if(msg.toUpperCase() == "PLUS"){
+        //     console.log("+");
+        //   }
+        //   else if(msg.toUpperCase() == "MINUS"){
+        //     console.log("-");
+        //   }
+            if (msg == answer){
+              console.log("Correct msg " + msg);
+            }
+            else{
+              console.log("Incorrect msg");
+            }
+         }
+    }
+
+    recognition.onerror = function(event) {
+      console.log("in onerror");
+      if (event.error == 'no-speech') {
+        console.log("no-speech");
+        start_img.src = 'mic.gif';
+        showInfo('info_no_speech');
+        ignore_onend = true;
       }
-  }
+      if (event.error == 'audio-capture') {
+        console.log("audio-capture");
+        start_img.src = 'mic.gif';
+        showInfo('info_no_microphone');
+        ignore_onend = true;
+      }
+      if (event.error == 'not-allowed') {
+        console.log("not-allowed");
+        // if (event.timeStamp - start_timestamp < 100) {
+        //   showInfo('info_blocked');
+        // } else {
+        //   showInfo('info_denied');
+        // }
+        ignore_onend = true;
+      }
+    }
 
-  recognition.onerror = function(event) {
-    console.log("in onerror");
-    if (event.error == 'no-speech') {
-      console.log("no-speech");
-      start_img.src = 'mic.gif';
-      showInfo('info_no_speech');
-      ignore_onend = true;
+    recognition.onend = function() {
+      recognizing = false;
+      if(ignore_onend){
+          return ;
+      }
+      if(!msg){
+          console.log("no msg");
+          return;
+      }
     }
-    if (event.error == 'audio-capture') {
-      console.log("audio-capture");
-      start_img.src = 'mic.gif';
-      showInfo('info_no_microphone');
-      ignore_onend = true;
-    }
-    if (event.error == 'not-allowed') {
-      console.log("not-allowed");
-      // if (event.timeStamp - start_timestamp < 100) {
-      //   showInfo('info_blocked');
-      // } else {
-      //   showInfo('info_denied');
-      // }
-      ignore_onend = true;
-    }
+    recognition.start();
   }
-
-  recognition.onend = function() {
-    recognizing = false;
-    if(ignore_onend){
-        return ;
-    }
-    if(!msg){
-        console.log("no msg");
-        return;
-    }
-  }
-  recognition.start();
 }
 
-function getMsg(){
+function ask_question(){
   // Start button
-  if (recognizing) {
-    recognition.stop();
-    return;
+  // if (recognizing) {
+  //   recognition.stop();
+  //   return;
+  // }
+  // final_transcript = '';
+  // recognition.lang = "en-US";
+  // recognition.start();
+  // ignore_onend = false;
+  var first = Math.floor(Math.random() * 10);
+  var second = Math.floor(Math.random() * 10);
+  var result = first + second;
+  var string_question = first + "+" + second;
+
+  var speech = new SpeechSynthesisUtterance();
+  speech.text = string_question;
+
+  console.log(speech);
+  speech.onend = function(){
+    console.log("speech end");
+    speech_recognition(result);
   }
-  final_transcript = '';
-  recognition.lang = "en-US";
-  recognition.start();
-  ignore_onend = false;
+  // speech.onerror = function(){
+  //   console.log("speech error");
+  // }
+
+  window.speechSynthesis.speak(speech);
 }
