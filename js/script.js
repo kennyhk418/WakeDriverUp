9790715a33da126e108adf579784bac98178a5f4
Tsz Him Kwan <himkwanbbq@hotmail.com>
2017-10-21 18:37:53 -0700

    add comment

--- js/script.js
+++ js/script.js
@@ -2,12 +2,12 @@ var recognizing = false;
 var ignore_onend;
 var msg = '';
 var start_time;
-function permission(){
+
+/* function name: askPermission */
+function askPermission(){
   navigator.getUserMedia (
      // constraints
-     {
-        audio: true
-     },
+     {audio: true},
 
      // successCallback
      function(localMediaStream) {
@@ -24,22 +24,31 @@ function permission(){
 }
 
 var loop_question = setInterval(function(){
-    var question = ask_question();
-    var correct = speech_recognition(question);
-  }, 5000);
+  var question = ask_question();
+  var correct = speech_recognition(question);
+}, 5000);
 
-// speech_recognition(temp);
-
+/* funciton name: speech_recognition 
+   get the response from the user
+*/
 function speech_recognition(question){
+  
+  /* the browser doesn't have speech recognition */
   if (!('webkitSpeechRecognition' in window)) {
     console.log("need webkitSpeechRecognition");
     //upgrade();
   }
+  
+  /* user has the speech recognition */
   else {
     console.log("has webkitSpeechRecognition");
-    permission();
+    
+    /* ask for audio permission first */
+    askPermission();
+    
     var recognition = new webkitSpeechRecognition();
     recognition.lang = "en-US";
+    
     //When user stop talking, recognition will end
     recognition.continuous = false;
     recognition.interimResults = true;
@@ -48,36 +57,39 @@ function speech_recognition(question){
       console.log("in onstart");
       start_time = (new Date()).getTime();
       recognizing = true;
+      
+      /* if within 6s after speech_recognition start,
+       * and no sound has been get, the recognition will be terminated
+       */
       var x = setInterval(function(){
-        // recognition.stop();
         recognition.stop();
         console.log("end onstart");
         clearInterval(x);
       },6000);
     }
+    
     recognition.onresult = function(event) {
-        console.log("in onresult");
-        msg = ''
-        if((new Date).getTime() - start_time > 5000){
-          console.log("expire");
-          return '';
+      console.log("in onresult");
+      msg = ''
+      /* if some sound is get, but already timeout, ignore the result */
+      if((new Date).getTime() - start_time > 5000){
+        console.log("expire");
+        return msg;
+      }
+      
+      /* try to reconize the words one by one and store into a string */
+      for (var i = event.resultIndex; i < event.results.length; i++){
+        if(event.results[i].isFinal){
+          msg += event.results[i][0].transcript;
         }
-        for (var i = event.resultIndex; i < event.results.length; i++){
-            if(event.results[i].isFinal){
-                msg += event.results[i][0].transcript;
-            }
-        }
-        console.log("msg = " + msg);
-         if(msg != ''){
-            document.getElementById('msg').innerHTML = msg;
-            correct = check_answer(question);
-            // if(correct){
-            //   return true;
-            // }
-            // else{
-            //   return false;
-            // }
-         }
+      }
+      console.log("msg = " + msg);
+      
+      /* some sound is get but not recongized, don't check with the answer */
+      if(msg != ''){
+        document.getElementById('msg').innerHTML = msg;
+        correct = check_answer(question);
+      }
     }
 
     recognition.onerror = function(event) {
@@ -103,18 +115,17 @@ function speech_recognition(question){
     recognition.onend = function() {
       recognizing = false;
       if(ignore_onend){
-          return ;
+        return ;
       }
       if(!msg){
-          console.log("no msg");
-          return;
+        console.log("no msg");
+        return;
       }
     }
     recognition.start();
   }
-  while(!correct){
-
-  }
+  
+  
   return correct;
 }
 
@@ -129,17 +140,25 @@ function check_answer(result){
     }
 }
 
+/* function name: ask_question 
+   generate two digits, speak, and return the addition of two digits
+*/
 function ask_question(){
   var first = Math.floor(Math.random() * 10);
   var second = Math.floor(Math.random() * 10);
   var result = first + second;
+  
+  /* string to be spoken */
   var string_question = first + "+" + second;
-
   var speech = new SpeechSynthesisUtterance();
+  
   speech.text = string_question;
 
   console.log(speech);
 
+  /* speak to the user */
   window.speechSynthesis.speak(speech);
+  
+  /* return the addition of two digits */
   return result;
 }
